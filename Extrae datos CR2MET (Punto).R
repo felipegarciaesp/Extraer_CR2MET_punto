b20b66a6a41ecb953654bb6c68a38104cbd282a3
Felipe Garcia <Felipe.Garcia@ausenco.com>
2026-02-14 12:11:30 -0300

    Modificaciones para proyecto Vicuna

--- Extrae datos CR2MET (Punto).R
+++ Extrae datos CR2MET (Punto).R
@@ -3,14 +3,18 @@ graphics.off()
 library(ncdf4)
 library(openxlsx)
 
-setwd("C:/Users/eduardo.loyola/OneDrive - Ausenco/Escritorio/Eduardo/Datos/Datos Grillados/CR2MET")
-name<-"CR2MET_pr_v2.5_day_1960_2021_005deg.nc"
-Salida<-'C:/Users/eduardo.loyola/OneDrive - Ausenco/Escritorio/Eduardo/Manto Verde/Trabajo/Datos CR2MET/PP_Las_Vegas.xlsx'
+# Ruta no tan larga y que no tengan un caracter especial
+# Los slash deben ser como el siguiente: "/"
+# Lo que debes cambiar en el codigo se encuentra indicado como comentario.
+
+setwd("C:/Codigos/Extraer_CR2MET_punto/CR2MET") # Ruta donde se ubica el netcdf. CAMBIAR
+name<-"CR2MET_tmin_v2_0_day_1979_2020_005deg.nc" #Nombre del archivo, debe tener extension. CAMBIAR
+Salida<-'C:/Codigos/Extraer_CR2MET_punto/tmin_Iglesia_Colorada.xlsx' # Archivo de salida. CAMBIAR
 nc<-nc_open(name)
-data <- ncvar_get(nc, varid = "pr")
+data <- ncvar_get(nc, varid = "tmin") # Variable a extraer, debe coincidir con nombre del archivo. CAMBIAR
 #Coordenadas del punto a evaluar
-QNlat<-(-32.706944)
-QNlon<-(-71.329166)
+QNlat<-(-28.1572) # Latitud en grados. CAMBIAR
+QNlon<-(-69.8808) # Longitud en grados. CAMBIAR
 
 #Seleccion de grillas
 corLat<-max(which(nc$dim$lat$vals<QNlat))
@@ -22,6 +26,7 @@ if ((QNlon-nc$dim$lon$vals[corLon])>(nc$dim$lon$vals[corLon+1]-QNlon)){
   corLon=corLon+1}
 
 aux<-as.data.frame(data[corLon,corLat, ])
-fecha<-seq(as.Date("1960-01-01"), length=dim(aux)[1], by="day")
+fecha<-seq(as.Date("1979-01-01"), length=dim(aux)[1], by="day") # Fecha inicio archivo netcdf. CAMBIAR
 
 write.xlsx(cbind(fecha,aux), file=Salida,overwrite = TRUE)
+
